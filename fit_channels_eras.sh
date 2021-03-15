3046d11f3ee31c9e2a5c5456abbf566aed190b09
ralfschmieder <ralf.schmieder@student.kit.edu>
2021-03-15 19:28:30 +0100

    update

--- fit_channels_eras.sh
+++ fit_channels_eras.sh
@@ -5,9 +5,13 @@
 #     do
 #         ./utils/fit_cmssw.sh ${era} ${ch} 500 3 500 110
 #     done 
-# done
-
-for job in 219 223 224 225
-do
-    ./CMSSW_10_2_16_UL/src/create_combined_limit_nmssm.sh 2016,2017,2018 all 1000 3 ${job}
-done
+# done #
+mkdir condor_inter_rand
+cd condor_inter_rand
+for tag in  pNN_interpol_signalrandom_mH1000 #pNN_interpol_signalrandom_mH1000 pNN_interpol_signaltrue_mH1000 pNN_all_signalrandom_mH1000#
+do 
+    for job in 203 204 205 206 207 208 209 210 211 212 213 214 215 216 217 218 219 220 221 222 223 224 225 226 227 228 229
+    do
+        .././CMSSW_10_2_16_UL/src/create_combined_limit_nmssm_condor_tag.sh 2016,2017,2018 all 1000 3 ${job} condor ${tag}
+    done
+done 
