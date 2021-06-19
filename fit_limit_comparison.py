709fc999c81c94eabee861b707b8e6db84734d21
ralfschmieder <ralf.schmieder@student.kit.edu>
2021-06-19 16:00:41 +0200

    summed: modified into normed sum and implemented pos. to plot all eras and nodes, fit_limit: modified to read joson to plot whiskers

--- fit_limit_comparison.py
+++ fit_limit_comparison.py
@@ -1,94 +1,67 @@
 import numpy as np 
 import matplotlib.pyplot as plt
-
-
-exp_limits_nicks = ["240_90" , 
+import json
+# import seaborn as sns
+# import pandas as pd
+def main():
+    files=[]
+    exp_limits_procs = ["240_90" , 
                 "280_120",
                 "320_110",
                 "360_80",
+                "360_190",
                 "400_190",
                 "450_120",
                 "500_90",
                 "550_150",
                 "600_130",
+                "600_400",
                 "700_150",
+                "700_500",
                 "800_400",
                 "900_300",
                 "1000_150",
                 "1000_800",
                 "1600_600",        
                 ]
-#10_onenet:14classes to fit
-# exp_limits_janek_10_onenet = np.array([
-#                                     [0.1062500000000000, 0.11171875],
-#                                     [0.10625,0.133984375],
-#                                     [0.0685546875,0.075390625],
-#                                     [0.02734375,0.04794921875],
-#                                     [0.0470703125,0.0583984375],
-#                                     [0.0132080078,0.0147216796875],
-#                                     [0.0079345703125,0.0107421875],
-#                                     [0.007958984375,0.011181640625],
-#                                     [0.00584716796875,0.0059326171875],
-#                                     [0.0041870117187500005,0.00472412109375],
-#                                     [0.0052734375,0.0078857421875],
-#                                     [0.00323486328125,0.00338134765625],
-#                                     [0.0029174804687,0.002734375],
-#                                     [0.016552734375,0.0142578125],
-#                                     [0.005053710937500001,0.004931640625]
-#                                     ]   
-#                                 )
 
-#68_onenet_120_60_6
-# exp_limits_janek_68_onenet_120_60_6 = np.array([
-#                                     [0.1062500000000000, 0.2296875],
-#                                     [0.10625,0.186328125],
-#                                     [0.0685546875,0.1083984375],
-#                                     [0.02734375,0.07578125],
-#                                     [0.0470703125,0.135546875],
-#                                     [0.0132080078,0.04169921875],
-#                                     [0.0079345703125,0.02099609375],
-#                                     [0.007958984375,0.02080078125],
-#                                     [0.00584716796875,0.013330078125],
-#                                     [0.0041870117187500005,0.009619140625],
-#                                     [0.0052734375,0.010400390625],
-#                                     [0.00323486328125,0.0061767578125],
-#                                     [0.0029174804687,0.00443115234375],
-#                                     [0.016552734375,0.033203125],
-#                                     [0.005053710937500001,0.0087646484375]
-#                                     ]   
-#                                 )
+    
 
-exp_limits_janek_10_onenet_1nmssmclass = np.array([
-                                    [0.1062500000000000, 0.32890625],
-                                    [0.10625,0.27421875],
-                                    [0.0685546875,0.134765625],
-                                    [0.02734375,0.076953125],
-                                    [0.0470703125,0.1201171875],
-                                    [0.0132080078,0.0318359375],
-                                    [0.0079345703125,0.019677734375],
-                                    [0.007958984375,0.01845703125],
-                                    [0.00584716796875,0.011572265625],
-                                    [0.0041870117187500005,0.008251953125],
-                                    [0.0052734375,0.0106201171875],
-                                    [0.00323486328125,0.00517578125],
-                                    [0.0029174804687,0.00391845703125],
-                                    [0.016552734375,0.018896484375],
-                                    [0.005053710937500001,0.006103515625]
-                                    ]   
-                                )
-
-ratio=np.hsplit(exp_limits_janek_10_onenet_1nmssmclass,2)[1]/np.hsplit(exp_limits_janek_10_onenet_1nmssmclass,2)[0]
-xaxis=np.arange(len(ratio))
-
-one=np.ones(10000)
-one_xs=np.linspace(-1,len(ratio)+1,10000)
-
-fig = plt.figure(figsize=[8, 8])
-plt.plot(one_xs,one,'--')
-plt.plot(xaxis,ratio, marker=".",markersize=6 ,linestyle="")
-plt.xticks(xaxis,exp_limits_nicks)
-plt.xticks(rotation = 45)
-plt.xlim(-1,len(ratio)+1)
-plt.ylabel("ratio")
-plt.savefig("plots/10_onenet_1nmssmclass/limits/ratio.png")
-#plt.show()
+    stats=[]
+    for procs in exp_limits_procs:
+        heavy_mass=procs.split("_")[0]+".0"
+        light_mass=procs.split("_")[1]+".0"
+        new_file= "fit_output/10_onenet/nmssm_all_1000_0_{procs}_cmb.json".format(procs=procs)
+        ref_file="/work/jbechtel/postprocessing/nmssm/train_all/limit_jsons/nmssm_combined_all_{hm}_cmb.json".format(hm=procs.split("_")[0])
+        with open(new_file,"r") as f:
+            in_dict = json.load(f)
+        with open(ref_file,"r") as f:
+            ref_dict = json.load(f)
+        temp_dict={}
+        temp_dict["label"]=procs
+        for key in ref_dict[light_mass].keys():
+            if 'exp-2' in key:
+                temp_dict["whislo"]=ref_dict[light_mass][key]/ref_dict[light_mass]["exp0"]  
+            elif 'exp-1' in key:
+                temp_dict["q1"]=ref_dict[light_mass][key]/ref_dict[light_mass]["exp0"]  
+            elif 'exp0' in key:
+                temp_dict["med"]=in_dict[light_mass][key]*0.1/ref_dict[light_mass][key]    
+            elif 'exp+1' in key:
+                temp_dict["q3"]=ref_dict[light_mass][key]/ref_dict[light_mass]["exp0"]  
+            elif 'exp+2' in key:
+                temp_dict["whishi"]=ref_dict[light_mass][key]/ref_dict[light_mass]["exp0"]  
+        stats.append(temp_dict)
+       # del new_dict[procs]['obs']
+    medianprops = dict(linestyle='-.', linewidth=2.5, color='firebrick')
+    fig,axes=plt.subplots(figsize=(8,8))
+    axes.bxp(stats,showfliers=False,vert=False,medianprops=medianprops)
+    plt.vlines(1,0.5, len(exp_limits_procs)+0.5,linestyles="dashed")
+    plt.xlabel("ratio",fontsize=16)
+    plt.ylabel(r'$\mathrm{m(H)}$'+'_'+r'$\mathrm{m(h_{S})}$',fontsize=16)
+    plt.xlabel("ratio",fontsize=16)
+    plt.subplots_adjust(bottom=.1, left=.15)
+    plt.savefig("plots/10_onenet/limits/ratio.png")
+    
+    
+if __name__ == "__main__":
+    main()
--- summed_2d_taylor_score.py
+++ summed_2d_taylor_score.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 
-parser = argparse.ArgumentParser(description='compare histograms in old and new framework in nmssm analysis')
+parser = argparse.ArgumentParser(description='sum up all taylor 1d and 2d scores')
 parser.add_argument(       
         "--era",
         required=True,
@@ -37,34 +37,57 @@ parser.add_argument(
     )
 args = parser.parse_args()
 
-if args.era == "all_eras":
-    emb=np.loadtxt("/work/rschmieder/nmssm_condor_analysis/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}/fold0_keras_taylor_ranking_emb_2016.txt".format(tag=args.tag,ch=args.channel,era=args.era),dtype=str,delimiter=":")
-    tt=np.loadtxt("/work/rschmieder/nmssm_condor_analysis/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}/fold0_keras_taylor_ranking_tt_2016.txt".format(tag=args.tag,ch=args.channel,era=args.era),dtype=str,delimiter=":")
-    ff=np.loadtxt("/work/rschmieder/nmssm_condor_analysis/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}/fold0_keras_taylor_ranking_ff_2016.txt".format(tag=args.tag,ch=args.channel,era=args.era),dtype=str,delimiter=":")
-    misc=np.loadtxt("/work/rschmieder/nmssm_condor_analysis/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}/fold0_keras_taylor_ranking_misc_2016.txt".format(tag=args.tag,ch=args.channel,era=args.era),dtype=str,delimiter=":")
-    nmssm=np.loadtxt("/work/rschmieder/nmssm_condor_analysis/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}/fold0_keras_taylor_ranking_NMSSM_2016.txt".format(tag=args.tag,ch=args.channel,era=args.era),dtype=str,delimiter=":")
-    for era in ["2017","2018"]:
-        emb_era=np.loadtxt("/work/rschmieder/nmssm_condor_analysis/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}/fold0_keras_taylor_ranking_emb_{era}.txt".format(tag=args.tag,ch=args.channel,era=era),dtype=str,delimiter=":")
-        tt_era=np.loadtxt("/work/rschmieder/nmssm_condor_analysis/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}/fold0_keras_taylor_ranking_tt_{era}.txt".format(tag=args.tag,ch=args.channel,era=era),dtype=str,delimiter=":")
-        ff_era=np.loadtxt("/work/rschmieder/nmssm_condor_analysis/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}/fold0_keras_taylor_ranking_ff_{era}.txt".format(tag=args.tag,ch=args.channel,era=era),dtype=str,delimiter=":")
-        misc_era=np.loadtxt("/work/rschmieder/nmssm_condor_analysis/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}/fold0_keras_taylor_ranking_misc_{era}.txt".format(tag=args.tag,ch=args.channel,era=era),dtype=str,delimiter=":")
-        nmssm_era=np.loadtxt("/work/rschmieder/nmssm_condor_analysis/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}/fold0_keras_taylor_ranking_NMSSM_{era}.txt".format(tag=args.tag,ch=args.channel,era=era),dtype=str,delimiter=":")
+def taylor_norm(taylor_ranks):
+    sum_taylor_ranks=0
+    for t in range(taylor_ranks.shape[0]):
+        sum_taylor_ranks+=float(taylor_ranks[t][2])
+    for t in range(taylor_ranks.shape[0]):
+        taylor_ranks[t][2]=float(taylor_ranks[t][2])/sum_taylor_ranks
+    return taylor_ranks
 
+if args.era == "all_eras":
+    emb=np.loadtxt("/work/rschmieder/nmssm_analysis_68trainings_gpu_py3/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}_500_3/fold0_keras_taylor_ranking_emb_2016.txt".format(tag=args.tag,ch=args.channel,era=args.era),dtype=str,delimiter=":")
+    tt=np.loadtxt("/work/rschmieder/nmssm_analysis_68trainings_gpu_py3/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}_500_3/fold0_keras_taylor_ranking_tt_2016.txt".format(tag=args.tag,ch=args.channel,era=args.era),dtype=str,delimiter=":")
+    ff=np.loadtxt("/work/rschmieder/nmssm_analysis_68trainings_gpu_py3/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}_500_3/fold0_keras_taylor_ranking_ff_2016.txt".format(tag=args.tag,ch=args.channel,era=args.era),dtype=str,delimiter=":")
+    misc=np.loadtxt("/work/rschmieder/nmssm_analysis_68trainings_gpu_py3/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}_500_3/fold0_keras_taylor_ranking_misc_2016.txt".format(tag=args.tag,ch=args.channel,era=args.era),dtype=str,delimiter=":")
+    nmssm=np.loadtxt("/work/rschmieder/nmssm_analysis_68trainings_gpu_py3/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}_500_3/fold0_keras_taylor_ranking_NMSSM_MH500_3_2016.txt".format(tag=args.tag,ch=args.channel,era=args.era),dtype=str,delimiter=":")
+    emb=taylor_norm(emb)
+    ff=taylor_norm(ff)
+    tt=taylor_norm(tt)
+    misc=taylor_norm(misc)
+    nmssm=taylor_norm(nmssm)
+    for era in ["2017","2018"]:
+        emb_era=np.loadtxt("/work/rschmieder/nmssm_analysis_68trainings_gpu_py3/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}_500_3/fold0_keras_taylor_ranking_emb_{era}.txt".format(tag=args.tag,ch=args.channel,era=era),dtype=str,delimiter=":")
+        tt_era=np.loadtxt("/work/rschmieder/nmssm_analysis_68trainings_gpu_py3/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}_500_3/fold0_keras_taylor_ranking_tt_{era}.txt".format(tag=args.tag,ch=args.channel,era=era),dtype=str,delimiter=":")
+        ff_era=np.loadtxt("/work/rschmieder/nmssm_analysis_68trainings_gpu_py3/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}_500_3/fold0_keras_taylor_ranking_ff_{era}.txt".format(tag=args.tag,ch=args.channel,era=era),dtype=str,delimiter=":")
+        misc_era=np.loadtxt("/work/rschmieder/nmssm_analysis_68trainings_gpu_py3/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}_500_3/fold0_keras_taylor_ranking_misc_{era}.txt".format(tag=args.tag,ch=args.channel,era=era),dtype=str,delimiter=":")
+        nmssm_era=np.loadtxt("/work/rschmieder/nmssm_analysis_68trainings_gpu_py3/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}_500_3/fold0_keras_taylor_ranking_NMSSM_MH500_3_{era}.txt".format(tag=args.tag,ch=args.channel,era=era),dtype=str,delimiter=":")
+        emb_era=taylor_norm(emb_era)
+        ff_era=taylor_norm(ff_era)
+        tt_era=taylor_norm(tt_era)
+        misc_era=taylor_norm(misc_era)
+        nmssm_era=taylor_norm(nmssm_era)
         emb=np.vstack((emb,emb_era))
         tt=np.vstack((tt,tt_era))
         ff=np.vstack((ff,ff_era))
         misc=np.vstack((misc,misc_era))
         nmssm=np.vstack((nmssm,nmssm_era))
 
-else: 
-    emb=np.loadtxt("/work/rschmieder/nmssm_condor_analysis/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}/fold0_keras_taylor_ranking_emb_{era}.txt".format(tag=args.tag,ch=args.channel,era=args.era),dtype=str,delimiter=":")
-    tt=np.loadtxt("/work/rschmieder/nmssm_condor_analysis/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}/fold0_keras_taylor_ranking_tt_{era}.txt".format(tag=args.tag,ch=args.channel,era=args.era),dtype=str,delimiter=":")
-    ff=np.loadtxt("/work/rschmieder/nmssm_condor_analysis/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}/fold0_keras_taylor_ranking_ff_{era}.txt".format(tag=args.tag,ch=args.channel,era=args.era),dtype=str,delimiter=":")
-    misc=np.loadtxt("/work/rschmieder/nmssm_condor_analysis/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}/fold0_keras_taylor_ranking_misc_{era}.txt".format(tag=args.tag,ch=args.channel,era=args.era),dtype=str,delimiter=":")
-    nmssm=np.loadtxt("/work/rschmieder/nmssm_condor_analysis/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}/fold0_keras_taylor_ranking_NMSSM_{era}.txt".format(tag=args.tag,ch=args.channel,era=args.era),dtype=str,delimiter=":")
 
+else: 
+    emb=np.loadtxt("/work/rschmieder/nmssm_analysis_68trainings_gpu_py3/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}_500_3/fold0_keras_taylor_ranking_emb_{era}.txt".format(tag=args.tag,ch=args.channel,era=args.era),dtype=str,delimiter=":")
+    tt=np.loadtxt("/work/rschmieder/nmssm_analysis_68trainings_gpu_py3/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}_500_3/fold0_keras_taylor_ranking_tt_{era}.txt".format(tag=args.tag,ch=args.channel,era=args.era),dtype=str,delimiter=":")
+    ff=np.loadtxt("/work/rschmieder/nmssm_analysis_68trainings_gpu_py3/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}_500_3/fold0_keras_taylor_ranking_ff_{era}.txt".format(tag=args.tag,ch=args.channel,era=args.era),dtype=str,delimiter=":")
+    misc=np.loadtxt("/work/rschmieder/nmssm_analysis_68trainings_gpu_py3/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}_500_3/fold0_keras_taylor_ranking_misc_{era}.txt".format(tag=args.tag,ch=args.channel,era=args.era),dtype=str,delimiter=":")
+    nmssm=np.loadtxt("/work/rschmieder/nmssm_analysis_68trainings_gpu_py3/sm-htt-analysis/output/ml/{tag}/all_eras_{ch}_500_3/fold0_keras_taylor_ranking_NMSSM_MH500_3_{era}.txt".format(tag=args.tag,ch=args.channel,era=args.era),dtype=str,delimiter=":")
+    emb=taylor_norm(emb)
+    ff=taylor_norm(ff)
+    tt=taylor_norm(tt)
+    misc=taylor_norm(misc)
+    nmssm=taylor_norm(nmssm)
+
+# print(np.mean(((emb[:,2].astype(np.float))/np.sum(emb[:,2].astype(np.float)))[0]+((tt[:,2].astype(np.float))/np.sum(tt[:,2].astype(np.float)))[0]+((ff[:,2].astype(np.float))/np.sum(ff[:,2].astype(np.float)))[0]+((misc[:,2].astype(np.float))/np.sum(misc[:,2].astype(np.float)))[0]+((nmssm[:,2].astype(np.float))/np.sum(nmssm[:,2].astype(np.float)))[0]))
 if "tt" in args.channel:
-    ranks=464
     vars=["pt_1"
     ,"pt_2"
     ,"m_vis"
@@ -90,12 +113,12 @@ if "tt" in args.channel:
     ,"kinfit_mh2"
     ,"kinfit_chi2"
     ,"highCSVjetUsedFordiBJetSystemCSV"
-    ,"NMSSM_light_mass"
+    #,"NMSSM_light_mass"
     ,"2016"
     ,"2017"
     ,"2018"]
+    ranks=len(vars)**2/2+len(vars)+len(vars)/2
 else:
-    ranks=324
     vars=["pt_1"
     ,"pt_2"
     ,"m_vis"
@@ -116,16 +139,16 @@ else:
     ,"kinfit_mH"
     ,"kinfit_mh2"
     ,"kinfit_chi2"
-    ,"NMSSM_light_mass"
+    #,"NMSSM_light_mass"
     ,"2016"
     ,"2017"
     ,"2018"]
+    ranks=len(vars)**2/2+len(vars)+len(vars)/2
 
 vars_dict={}
 for var in vars:
     vars_dict[var]=[]
 if args.node == "all":
-    norm=5*len(vars)*ranks
     taylor_ranks=np.vstack((emb,tt,ff,misc,nmssm))
 else:
     norm=len(vars)*ranks
@@ -139,8 +162,7 @@ else:
         taylor_ranks=misc
     elif args.node=="nmssm":
         taylor_ranks=nmssm
-if args.era =="all_eras":
-    norm=norm*3
+
 for key in vars_dict:
     for t in range(taylor_ranks.shape[0]):
         if taylor_ranks[t][1].find(",") != -1:
@@ -154,12 +176,18 @@ for key in vars_dict:
 sums={}
 for i,key in enumerate(vars_dict.keys()):
     sums[key]=sum(vars_dict[key])
-sort=sorted(sums.items(), key=lambda item: item[1],reverse=True)
+
+sort=sorted(sums.items(), key=lambda item: item[1])
 sorted_dict = {k: v for k, v in sort}
-plt.plot(range(len(sorted_dict)),list(sorted_dict.values()),marker=".",markersize=6 ,linestyle="")
-plt.xticks(range(len(sorted_dict)),list(sorted_dict.keys()))
-plt.xticks(rotation = 45)
+
+normed_summed_coeff=np.asarray(list(sorted_dict.values()))/np.sum(np.asarray(list(sorted_dict.values())))
+
+plt.plot(normed_summed_coeff,range(len(sorted_dict)),marker=".",markersize=7 ,linestyle="")
+plt.yticks(range(len(sorted_dict)),list(sorted_dict.keys()))
 fig = plt.gcf()
 fig.set_size_inches(18.5, 10.5)
-plt.savefig("et16emb.png")
-plt.show()
+plt.xlabel("normalized sum of taylor coefficients",fontsize=16)
+plt.ylabel("variable",fontsize=16)
+plt.savefig("plots/{tag}/summed_2d_taylorscore_{era}_{channel}_{node}.png".format(tag=args.tag,era=args.era,channel=args.channel,node=args.node))
+plt.savefig("plots/{tag}/summed_2d_taylorscore_{era}_{channel}_{node}.pdf".format(tag=args.tag,era=args.era,channel=args.channel,node=args.node))
+#plt.show()
